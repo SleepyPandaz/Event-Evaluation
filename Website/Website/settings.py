36d61f862af013ed147ca3c37313de56b7203ebd
ZakLuetmer <zvluetmer@csbsju.edu>
2018-10-02 16:08:18 -0500

    Added DB

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'Website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'C:/Users/ZakLuetmer/desktop/SQlite/CSCI330.db',
     }
 }
 
