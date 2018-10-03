1d71f205dbcf263724aa159875bdaa3bc5e38d96
vuexiong <vuexiong@152.65.66.76>
2018-10-02 19:59:22 -0500

    updating files with group

--- Website/Groups/views.py
+++ Website/Groups/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls X2 index.")
+
+#def groups(request):
+   # return render(request, 'Groups/group.html', {})
+
+
+#def sort(gender:String,age:Numbers,major:String):
+   # student = gender,age,major
+   # return student
