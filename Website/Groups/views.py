810f9e9de50d08e4c62492ddbac9c2497aa40d9b
vuexiong <vuexiong@152.65.66.76>
2018-10-12 08:46:36 -0500

    made small changes

--- Website/Groups/views.py
+++ Website/Groups/views.py
@@ -7,28 +7,18 @@ from django.shortcuts import HttpResponse
 from .models import Student,Events
 # Create your views here
 
-#class GroupsPageView(TemplateView):
-   # template_name = "group.html"
-   # def view(self,request, **kwargs):
-       # return render(request,'group.html',context=None)
-
-    #for x in range(0,10):
-#    def group(request):
-  #      return HttpResponse("Hello, world. You're at the polls X2 index.")
-
-#def groups(request):
-   # return render(request, 'Groups/group.html', {})
-
-
-#def sort(gender:String,age:Numbers,major:String):
-   # student = gender,age,major
-   # return student
-   
-   
 def index(request):
   html = "<H1>Groups</H1><HR>"
   #template = loader.get_template('Events/index.html')
   #template = loader.get_template('Events/general_template.html')
   return render(request,'Groups/group.html')
   #return render(request,'Events/general_template.html')
-  
+
+def list(request):
+    student_name = Student.objects.all()
+    event_name = Events.objects.all()
+    context= {
+        'student_name' : student_name,
+        'event_name': event_name
+        } 
+    return render(request,'Groups/group.html',context)
