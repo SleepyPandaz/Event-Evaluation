2d073808d74a5ce69ac50b93af66697329fb807b
vuexiong <vuexiong@152.65.66.231>
2018-10-28 20:19:33 -0500

    updated models with new fields, made samll changes in general templates

--- Website/Groups/models.py
+++ Website/Groups/models.py
@@ -3,9 +3,12 @@ import datetime
 from unittest.util import _MAX_LENGTH
 # Create your models here.
 class Student(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=400)
     major = models.CharField(max_length=200)
     year = models.IntegerField()
+    race_Ethnicity = models.CharField(max_length=800)
+    international= models.CharField(max_length=100)
+    campus = models.CharField(max_length=300)
     def __str__(self):
         return '%s' % (self.name)
 class Events(models.Model):
