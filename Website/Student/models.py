6ab255686fc4b6ebdf668d8413b6d53c0198369c
jubiealade <jubiealade4@gmail.com>
2018-11-02 12:55:42 -0500

    Improving the Student table. Additions now show up in the table

--- Website/Student/models.py
+++ Website/Student/models.py
@@ -4,6 +4,9 @@ from unittest.util import _MAX_LENGTH
 class Name(models.Model):   
     firstName = models.CharField(max_length=30)
     lastName = models.CharField(max_length=30)
+    studentId = models.CharField(max_length=15)
+    numEvents = models.CharField(max_length = 3)
+    numSurveys = models.CharField(max_length = 3)
     def __str__(self):
         return '%s' % (self.firstName)
    
