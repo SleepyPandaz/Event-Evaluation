46f540d9196f18ffb48e756aba325bd65c6be10e
jubiealade <jubiealade4@gmail.com>
2018-11-01 21:23:08 -0500

    adding template for student

--- Website/Student/models.py
+++ Website/Student/models.py
@@ -3,9 +3,9 @@ from unittest.util import _MAX_LENGTH
 
     
 class Name(models.Model):   
-    first_Name = models.CharField(max_length=30)
-    last_Name = models.CharField(max_length=30)
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
     def __str__(self):
-        return '%s' % (self.first_Name)
+        return '%s' % (self.firstName)
    
     
