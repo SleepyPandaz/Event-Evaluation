04c8a0492983917a605b9946bbbf0e70ac2b7c49
Karld <zbai001@csbsju.edu>
2018-12-09 21:14:50 -0600

    reading data from views

--- Website/Event_Statistics/views.py
+++ Website/Event_Statistics/views.py
@@ -15,9 +15,8 @@ def index(request):
 def list(request):
     ESList = Event_Statistics.objects.all()
     data = request.session['data_set']
-    print(data)
-    file=open("Event_Statistics\dataFiles\Alphabet_Soup_Evaluation.csv",'r')
-    data=file.read().split("\n")
+    
+    data=data.split("\n")
     context = {
         'ESList': ESList,
         'data':data,
